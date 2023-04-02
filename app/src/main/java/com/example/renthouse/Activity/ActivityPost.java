14ae675b46636e75c04554005f9bb7a7280c9a34
nhubaole <okamisameko7123@gmail.com>
2023-04-02 15:13:04 +0700

    Add Post for rent (location) UI

--- app/src/main/java/com/example/renthouse/Activity/ActivityPost.java
+++ app/src/main/java/com/example/renthouse/Activity/ActivityPost.java
@@ -11,6 +11,6 @@ public class ActivityPost extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_information);
+        setContentView(R.layout.activity_post_location);
     }
 }
