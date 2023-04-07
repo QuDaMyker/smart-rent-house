2beb4829d645ba5700b28f6717a87dfc401c14de
NguyenCaoHoai-windows <21520238@gm.uit.edu.vn>
2023-04-07 09:51:15 +0700

    commit

--- app/src/main/java/com/example/renthouse/Activity/ActivityFilter.java
+++ app/src/main/java/com/example/renthouse/Activity/ActivityFilter.java
@@ -2,9 +2,14 @@ package com.example.renthouse.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
 
 import com.example.renthouse.R;
+
 
 public class ActivityFilter extends AppCompatActivity {
 
@@ -12,5 +17,15 @@ public class ActivityFilter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        Button buttonPrice = findViewById(R.id.buttonPrice);
+
+        buttonPrice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HorizontalScrollView horizontalScrollViewResult = findViewById(R.id.horizontalDisplayResult);
+                horizontalScrollViewResult.setVisibility(View.GONE);
+            }
+        });
     }
 }
