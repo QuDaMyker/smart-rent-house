c5d390294ba5c2f6bd6d005e7e4e9fee69db265d
nhubaole <okamisameko7123@gmail.com>
2023-04-11 10:40:07 +0700

    Convert Activity into Fragment

--- app/src/main/java/com/example/renthouse/Activity/ActivityPost.java
+++ app/src/main/java/com/example/renthouse/Activity/ActivityPost.java
@@ -13,69 +13,10 @@ import com.google.android.material.button.MaterialButton;
 
 public class ActivityPost extends AppCompatActivity {
 
-    String[] buttonTitles = {
-            "WC riêng",
-            "Cửa sổ",
-            "Wifi",
-            "Nhà bếp",
-            "Máy giặt",
-            "Tủ lạnh",
-            "Chỗ để xe",
-            "An ninh",
-            "Tự do",
-            "Chủ riêng",
-            "Gác lửng",
-            "Thú cưng",
-            "Giường",
-            "Tủ đồ",
-            "Máy lạnh"
-    };
-
-    int[] iconIds = {
-            R.drawable.ic_wc,
-            R.drawable.ic_window,
-            R.drawable.ic_wifi,
-            R.drawable.ic_kitchen,
-            R.drawable.ic_laundry,
-            R.drawable.ic_fridge,
-            R.drawable.ic_motobike,
-            R.drawable.ic_security,
-            R.drawable.ic_timer,
-            R.drawable.outline_person_24,
-            R.drawable.ic_entresol,
-            R.drawable.ic_pet,
-            R.drawable.ic_bed,
-            R.drawable.ic_wardrobe,
-            R.drawable.ic_cool
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_confirm);
+        setContentView(R.layout.activity_post);
         //createButtons();
-    }
-
-    private void createButtons() {
-        GridLayout gridLayout = findViewById(R.id.grid_layout);
-
-        for (int i = 0; i < buttonTitles.length; i++) {
-            MaterialButton button = new MaterialButton(this);
-            button.setPadding(15,0,15,0);
-            button.setText(buttonTitles[i]);
-            button.setIcon(getDrawable(iconIds[i]));
-            button.setCheckable(true);
-            button.setIconTint(getColorStateList(R.color.button_text_selector));
-            button.setTextColor(getColorStateList(R.color.button_text_selector));
-            button.setTextSize(16);
-            button.setBackgroundTintList(getColorStateList(R.color.button_background_selector));
-            button.setIconGravity(MaterialButton.ICON_GRAVITY_TEXT_START);
-            button.setCornerRadius(16);
-            button.setLayoutParams(new GridLayout.LayoutParams(
-                    GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f),
-                    GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f)
-            ));
-            gridLayout.addView(button);
-        }
     }
 }
---
+++ app/src/main/java/com/example/renthouse/Activity/FragmentPost/FragmentConfirm.java
@@ -0,0 +1,19 @@
+package com.example.renthouse.Activity.FragmentPost;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+import com.example.renthouse.R;
+
+public class FragmentConfirm extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_post_confirm, container, false);
+    }
+}
