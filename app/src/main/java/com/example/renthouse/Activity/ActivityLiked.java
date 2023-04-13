a0aa7640551182fea4f4ce6fee6a9c9e808135d3
Nhongnhong-0101 <21522423@gm.uit.edu.vn>
2023-04-13 16:01:13 +0700

    Fix

--- app/src/main/java/com/example/renthouse/Activity/ActivityLiked.java
+++ app/src/main/java/com/example/renthouse/Activity/ActivityLiked.java
@@ -1,40 +1,41 @@
 package com.example.renthouse.Activity;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
-
 import com.example.renthouse.R;
-import com.example.renthouse.databinding.ActivityLikedEmptyBinding;
+import com.example.renthouse.databinding.ActivityLikedBinding;
 
 public class ActivityLiked extends AppCompatActivity {
 
-    ActivityLikedEmptyBinding binding;
+    ActivityLikedBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLikedEmptyBinding.inflate(getLayoutInflater());
+        binding = ActivityLikedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.navigation.setOnItemSelectedListener(item -> {
 
-        binding.navigation.setOnItemSelectedListener( item ->{
+           switch (item.getItemId()){
+               case R.id.btnLiked:
+                   replaceFragment(new LikedEmptyFragment());
 
-            switch (item.getItemId()) {
-                case R.id.btnLiked:
-                    replaceFragment(new LikedEmptyFragment());
-                    break;
-            }
+                   break;
+           }
+
             return true;
         });
-
     }
-    private void replaceFragment (Fragment f){
-        FragmentManager fn = getSupportFragmentManager();
-        FragmentTransaction ft = fn.beginTransaction();
-        ft.replace(R.id.frameLayout, f);
-        ft.commit();
+    private void replaceFragment(Fragment f)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, f);
+        fragmentTransaction.commit();
     }
 }
