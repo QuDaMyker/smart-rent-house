e918c7fd6148a249c6395d8ab4a7985541e0cec2
Anh Hà Mai <21520003@ms.uit.edu.vn>
2023-04-20 20:58:05 +0700

    Home_Account_Layout

---
+++ app/src/main/java/com/example/renthouse/Activity/ActivityAccount.java
@@ -0,0 +1,15 @@
+package com.example.renthouse.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.renthouse.R;
+
+public class ActivityAccount extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_personal);
+    }
+}
