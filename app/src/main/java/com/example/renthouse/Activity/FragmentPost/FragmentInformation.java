a906a9c93893d98138b38df3024217c9e1403ef9
nhubaole <okamisameko7123@gmail.com>
2023-05-07 13:51:53 +0700

    fix UI

--- app/src/main/java/com/example/renthouse/Activity/FragmentPost/FragmentInformation.java
+++ app/src/main/java/com/example/renthouse/Activity/FragmentPost/FragmentInformation.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,9 +14,27 @@ import androidx.fragment.app.Fragment;
 import com.example.renthouse.R;
 
 public class FragmentInformation extends Fragment {
+
+    CheckBox cbParking;
+    LinearLayout parkingInfo;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_post_information, container, false);
+        View v = inflater.inflate(R.layout.fragment_post_information, container, false);
+        cbParking = (CheckBox) v.findViewById(R.id.cbParking);
+        parkingInfo = (LinearLayout) v.findViewById(R.id.parkingInfo);
+
+        cbParking.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b == true){
+                    parkingInfo.setVisibility(View.VISIBLE);
+                }
+                else{
+                    parkingInfo.setVisibility(View.GONE);
+                }
+            }
+        });
+        return v;
     }
 }
