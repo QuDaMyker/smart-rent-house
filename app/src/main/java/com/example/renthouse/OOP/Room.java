3b5b9c8d77abdbd9534d2da6561fb74e366b45ca
nhubaole <okamisameko7123@gmail.com>
2023-07-12 11:52:34 +0700

    Fix bug

--- app/src/main/java/com/example/renthouse/OOP/Room.java
+++ app/src/main/java/com/example/renthouse/OOP/Room.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Room implements Serializable {
     private String id;
-    private static String title;
+    private String title;
     private String description;
     private String roomType;
     private int capacity;
@@ -64,7 +64,7 @@ public class Room implements Serializable {
         this.id = id;
     }
 
-    public static String getTitle() {
+    public String getTitle() {
         return title;
     }
 
